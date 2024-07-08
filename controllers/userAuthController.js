ce50e430f28b5cbe5bbd4a798ca7fd75db7df288
Elochukwu3 <oguajuromauld5@gmail.com>
2024-07-08 14:44:18 +0100

    fix auth controller error

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -1,26 +1,27 @@
-const User = require("../models/Users");
-const Organisation = require("../models/Orgs");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+const User = require("../models/Users");
+const Organisation = require("../models/Orgs");
+const {validateUser, validateLogin} = require("../helper/validation");
+
 
 async function userRegister(req, res) {
   const { firstName, lastName, email, password, phone } = req.body;
-  if (!firstName || !lastName || !email || !password || !phone) {
-    x``;
+  const { error } = validateUser(req.body);
+
+  if (error) {
     return res.status(422).send({
-      errors: [
-        {
-          field: "string",
-          message: "All fields are required",
-        },
-      ],
+      errors: error.details.map(err => ({
+        field: err.context.key,
+        message: err.message
+      }))
     });
   }
 
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({email})
     if (existingUser) {
-      return res.status(400).send({
+      return res.status(400).json({
         errors: [
           {
             field: "email",
@@ -31,6 +32,7 @@ async function userRegister(req, res) {
     }
     const hashedPassword = await bcrypt.hash(password, 12);
 
+    
     const newUser = await User.create({
       userId: new mongoose.Types.ObjectId(),
       firstName,
@@ -46,33 +48,41 @@ async function userRegister(req, res) {
       users: [newUser._id],
     });
 
-    const token = newUser.accesstoken();
-
-    res.status(201).send({
+    const token = newUser.accessToken();
+    const {__v, _id, password:pwd, ...remDetails} = newUser._doc;
+    return res.status(201).json({
         status: 'success',
         message: 'Registration successful',
         data: {
             accessToken: token,
-            user: { userId: user.userId, firstName: user.firstName, lastName: user.lastName, email: user.email, phone: user.phone }
+            user: {...remDetails} ,
         }
     });
   } catch (error) {
-    res.status(400).send({ status: 'Bad request', message: 'Registration unsuccessful', statusCode: 400 });
+    return res.status(400).json({ status: 'Bad request', message: 'Registration unsuccessful', statusCode: 400 });
   }
 }
 
 async function userLogin(req, res)  {
     const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(422).send({ errors: [{ field: 'fields', message: 'All fields are required' }] });
+    const { error } = validateLogin(req.body);
+
+    if (error) {
+      return res.status(422).send({
+        errors: error.details.map(err => ({
+          field: err.context.key,
+          message: err.message
+        }))
+      });
     }
+  
     try {
         const user = await User.findOne({ email });
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).send({ status: 'Bad request', message: 'Authentication failed', statusCode: 401 });
         }
 
-        const token = user.accesstoken();
+        const token = user.accessToken();
 
         res.status(200).send({
             status: 'success',
@@ -83,6 +93,8 @@ async function userLogin(req, res)  {
             }
         });
     } catch (error) {
+
+      
         res.status(400).send({ status: 'Bad request', message: 'Authentication failed', statusCode: 401 });
     }
 }
