33fb7bfeb040c34ba64205ac2dfb4cfbb39919c6
Elochukwu3 <oguajuromauld5@gmail.com>
2024-07-09 21:07:22 +0100

    clean up

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app'); // Adjust the path as needed
+const app = require('../app');
 
 describe('User Registration and Login', () => {
     it('Should register user successfully with default organisation', async () => {
